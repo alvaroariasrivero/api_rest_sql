3162ec1f136554b0654c0fc367667108fcc5ac34
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-05-20 10:47:50 +0200

    auth with jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,54 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt_secret = process.env.ULTRA_SECRET_KEY;
 
 const signUp = async(req, res) => {
     let data;
     try {
-        console.log(req.body.username)
         const hashPassword = await bcrypt.hash(req.body.password, saltRounds);
-        data = await User.createUser(req.body.username, req.body.email, hashPassword)
+        data = await User.createUser(req.body.username, req.body.email, hashPassword);
         res.status(201).json(data)
     } catch (error) {
         res.status(400).json({"error":error})
     }
-} 
+};
+
+const loginUser = async(req, res) => {
+    let data;
+    try {
+        const {email, password} = req.body
+        data = await User.existUser(email);
+        if(!data){
+            res.status(400).json({ msg: 'Incorrect user or password'}); 
+        }else{
+            const match = await bcrypt.compare(password, data.password);
+            if(match){
+                await User.setLoggedTrue(req.body.email)
+                const {email, username} = data;
+                const userForToken = {
+                    email: email,
+                    username: username,
+                };
+                const token = jwt.sign(userForToken, jwt_secret, {expiresIn: '20m'});
+                res
+                .status(200)
+                .json({
+                    msg:'Correct authentication',
+                    token: token});
+            }else {
+                res.status(400).json({ msg: 'Incorrect user or password'});
+            }
+        }        
+    } catch (error) {
+        console.log('Error:', error);
+    }
+};
 
 const user = {
-    signUp
+    signUp,
+    loginUser
 }
 
 module.exports = user
--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const userApi = require('../controllers/user');
 
 routes.post('/signup', userApi.signUp);
+routes.post('/login', userApi.loginUser);
 
 module.exports = routes;
