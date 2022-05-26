a817473e2d12a93f857829782bbc824680a98b8a
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-05-26 10:22:43 +0200

    added jwt middleware

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 const postApi = require('../controllers/post');
+const protectedRoutes = require('../middlewares/verifiedToken');
 
-routes.post('/post', postApi.createPost);
+routes.post('/post', protectedRoutes, postApi.createPost);
 routes.get('/post/:email?', postApi.getPost);
 
 module.exports = routes
